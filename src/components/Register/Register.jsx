de82cf62e4d5aa3df5063c3e6de0a5d433fb7f46
mehjabin hasan <mehjabinhasanmoni@gmail.com>
2023-04-18 04:43:20 +0600

    Show Email and Password from field

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Register = () => {
+const [email, setEmail] = useState('');
+
+
+    const handelSubmit = (event) =>{
+        event.preventDefault();
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+
+        // console.log(event.target.email.value);
+        // console.log(event.target.password.value);
+        console.log("Email & Password are : ",email, password);
+
+    }
+
+
+    const handelEmailChange = (event) => {
+            console.log(event.target.value);
+            setEmail(event.target.value);
+    }
+
+    const handelPasswordBlur = (event) =>{
+        console.log(event.target.value);
+    }
     return (
         <div>
             <h2> Please Register</h2>
-            <form>
-                <input type="email" name="email" id="email" placeholder='Your Email'/>
+            <form onSubmit={handelSubmit}>
+                <input onChange={handelEmailChange} type="email" name="email" id="email" placeholder='Your Email'/>
                 <br />
-                <input type="password" name="password" id="password" placeholder='Your Password' />
+                <input onBlur={handelPasswordBlur} type="password" name="password" id="password" placeholder='Your Password' />
                 <br />
                 <input type="submit" value="Register" />
             </form>
